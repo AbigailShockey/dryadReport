8a8bfcf97a88e47e097a3f3a0f5a7c0eb0506840
AbigailShockey <abbashockey@gmail.com>
2019-09-06 14:06:59 -0500

    Added R scripts

---
+++ render_report.R
@@ -0,0 +1,13 @@
+library(rmarkdown)
+
+# Get command line arguments
+args = commandArgs(trailingOnly=TRUE)
+
+# Read inputs
+species <- args[1]
+num.iso <- args[2]
+snp.mat <- read.table(args[3], header = T)
+nwk <- args[4]
+
+# Render the report
+render('data/anlaysis_report.Rmd', output_file='analysis_report.html')
