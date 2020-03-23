c60981c5c4fc8b6fc1d1f115b4e4ec7a2729423f
Abigail Shockey <abbashockey@gmail.com>
2020-03-23 12:14:11 -0500

    Update render_report.R

--- scripts/render_report.R
+++ scripts/render_report.R
@@ -12,7 +12,13 @@ snp_mat <- read.table(file_path_as_absolute(args[2]), header = T)
 nwk <- file_path_as_absolute(args[3])
 output_path <- args[4]
 ar_df <- args[5]
+if (ar_df != "none") {
+ read.table(file_path_as_absolute(ar_df, header = T, stringsAsFactors = F) 
+ }
 ar_mat <- args[6]
+if (ar_mat != "none") {
+ read.table(file_path_as_absolute(ar_mat, header = F, stringsAsFactors = F) 
+ }
 
 # Render the report
 render('analysis_report.Rmd', output_file='analysis_report.pdf')
